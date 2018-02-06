3fea5ae5a69bce8b67985c3c9ad789e1e1c2961c
camille-s <camille.seaberry@gmail.com>
2018-02-06 11:46:12 -0500

    Switch to leaflet & clean up

--- src/components/CityMap.jsx
+++ src/components/CityMap.jsx
@@ -1,71 +1,63 @@
 import React from 'react';
-import { Mercator } from '@vx/geo';
-import { ScaleSVG } from '@vx/responsive';
-import { LegendThreshold } from '@vx/legend';
 import * as topojson from 'topojson-client';
 import { format } from 'd3-format';
-import Tooltip from 'react-portal-tooltip';
+import { Map, TileLayer, GeoJSON } from 'react-leaflet';
+import Legend from './Legend';
 
 import topology from './hfd_shape.json';
 import '../styles/CityMap.css';
 
-const center = [-72.7192, 41.758];
+const b = topojson.bbox(topology);
+const bbox = [[ b[1], b[0] ], [ b[3], b[2] ]];
 const shape = topojson.feature(topology, topology.objects.hfd_shape);
 const mesh = [topojson.mesh(topology, topology.objects.hfd_shape, (a, b) => a.properties.Town !== b.properties.Town)];
 const merge = [topojson.merge(topology, topology.objects.hfd_shape.geometries)];
 
-const tipStyle = {
-	style: {
-		background: '#333',
-		opacity: 0.85,
-		boxShadow: 0,
-		color: 'white',
-		fontFamily: 'Barlow',
-		fontSize: '0.9em'
-	},
-	arrowStyle: {
-		color: '#333',
-		opacity: 0.85,
-		borderColor: false
-	}
+const cityStyle = {
+	fillColor: 'transparent',
+	color: '#333',
+	weight: 1.5,
+	pointerEvents: 'none'
 };
 
 export default class CityMap extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			tipString: '',
-			hovering: false,
-			hoverOver: 'path-asylumhill'
-		};
-		this.showTooltip = this.showTooltip.bind(this);
-		this.hideTooltip = this.hideTooltip.bind(this);
-	}
 
 	updateColor = (geography) => {
 		let name = geography.properties.Neighborhood;
-		return this.props.data[name] ? this.props.color(this.props.data[name].value) : '#ccc';
+		let color = this.props.data[name] ? this.props.color(this.props.data[name].value) : '#ccc';
+
+		return {
+			fillColor: color,
+			color: '#eee',
+			weight: 1,
+			opacity: 1,
+			fillOpacity: 0.75
+		};
 	};
 
-	makeId = (geography) => {
-		let name = geography.properties.Neighborhood.toLowerCase().replace(/\W/gi, '');
-		return `path-${name}`;
+	onEachFeature = (feature, layer) => {
+		let name = feature.properties.Neighborhood;
+
+		layer.on('click', this.props.handleClick)
+			.on('mouseover', this.addHilite)
+			.on('mouseout', this.removeHilite);
+		layer.bindTooltip(() => {
+			return this.props.data[name] ? `${name}: ${this.props.data[name].displayVal}` : `${name}: N/A`;
+		}, { direction: 'top', offset: [0, -20], className: 'custom-tip' });
 	};
 
-	showTooltip = (geography, event) => {
-		let name = geography.properties.Neighborhood;
-		let string = this.props.data[name] ? `${name}: ${this.props.data[name].displayVal}` : `${name}: N/A`;
-		let id = `path-${name.toLowerCase().replace(/\W/gi, '')}`;
-		this.setState({
-			tipString: string,
-			hovering: true,
-			hoverOver: id
+	addHilite = (e) => {
+		e.target.setStyle({
+			fillOpacity: 95,
+			weight: 1
 		});
+		// .bringToFront();
 	};
 
-	hideTooltip = () => {
-		this.setState({
-			hovering: false
+	removeHilite = (e) => {
+		e.target.setStyle({
+			fillOpacity: 0.75,
+			weight: 0.5
 		});
 	};
 
@@ -74,12 +66,43 @@ export default class CityMap extends React.Component {
 	}
 
 	render() {
-		let width = this.props.width;
-		let height = width * 0.9;
-
 		return (
 			<div className="CityMap" id="map">
-				<ScaleSVG width={width} height={height}>
+				<Map
+					bounds={bbox}
+					scrollWheelZoom={false}
+					zoomSnap={0.25}
+					zoomDelta={0.25}
+				>
+					<TileLayer
+						url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.{ext}"
+						attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+						subdomains='abcd'
+						minZoom={0}
+						maxZoom={20}
+						ext='png'
+						opacity={0.4}
+					/>
+					{/* neighborhoods */}
+					<GeoJSON
+						data={shape}
+						key={(feature) => feature.properties.Neighborhood}
+						style={this.updateColor}
+						onEachFeature={this.onEachFeature}
+					/>
+					<GeoJSON
+						data={mesh}
+						style={cityStyle}
+						interactive={false}
+					/>
+					<GeoJSON
+						data={merge}
+						style={cityStyle}
+						interactive={false}
+					/>
+				</Map>
+				<Legend colorscale={this.props.color} />
+				{/* <ScaleSVG width={width} height={height}>
 					<Mercator
 						data={shape.features}
 						id={this.makeId}
@@ -89,13 +112,13 @@ export default class CityMap extends React.Component {
 						stroke={'#777'}
 						fill={this.updateColor}
 						onClick={(geography) => (event) => {
-							this.props.handleClick(geography);
+					this.props.handleClick(geography);
 						}}
 						onMouseEnter={(geography) => (event) => {
-							this.showTooltip(geography, event);
+					this.showTooltip(geography, event);
 						}}
 						onMouseLeave={(geography) => (event) => {
-							this.hideTooltip();
+					this.hideTooltip();
 						}}
 						className="filled-map"
 					/>
@@ -119,9 +142,9 @@ export default class CityMap extends React.Component {
 						fill={'transparent'}
 						className="empty-map"
 					/>
-				</ScaleSVG>
+					</ScaleSVG>
 
-				<div className="legend-container"
+					<div className="legend-container"
 					style={{
 						position: this.props.collapse ? 'relative' : 'absolute',
 						bottom: '3em'
@@ -134,18 +157,18 @@ export default class CityMap extends React.Component {
 						shapeMargin="1px 0 0"
 						labelFormat={this.percentFormat}
 					/>
-				</div>
+					</div>
 
-				<Tooltip
+					<Tooltip
 					active={this.state.hovering}
 					position="top"
 					arrow="center"
 					parent={`#${this.state.hoverOver}`}
 					style={tipStyle}
 					tooltipTimeout={300}
-				>
+					>
 					<div className="tooltip-content">{this.state.tipString}</div>
-				</Tooltip>
+				</Tooltip> */}
 			</div>
 		);
 	}
